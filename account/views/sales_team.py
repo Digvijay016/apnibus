b6c648e7fa90701fe31f1d712a18cc3248571a0f
Digvijay016 <diggy@bu.edu>
2023-07-04 12:23:34 +0530

    Adding common response syntax for OTP

--- account/views/sales_team.py
+++ account/views/sales_team.py
@@ -16,7 +16,7 @@ from utils.restful_response import send_response
 from account.models.user_authentication import UserAuthenticationOTP
 from account.models.sales_team import SalesTeamUser
 from account.models.user import User
-from rest_framework import viewsets,generics
+from rest_framework import viewsets, generics
 from utils.apnibus_logger import apnibus_logger
 # from bus.models import Bus
 from rest_framework.authentication import TokenAuthentication
@@ -36,7 +36,8 @@ class UserAuthOTPViewset(viewsets.ModelViewSet):
 
     def create(self, request, *args, **kwargs):
         mobile = request.data.get('mobile', None)
-        internal_user_obj,created = SalesTeamUser.objects.get_or_create(mobile=mobile, type=SalesTeamUser.SALES)
+        internal_user_obj, created = SalesTeamUser.objects.get_or_create(
+            mobile=mobile, type=SalesTeamUser.SALES)
         print('################', internal_user_obj)
         if not internal_user_obj:
             mobile_number = request.data.get('mobile_number', None)
@@ -48,13 +49,13 @@ class UserAuthOTPViewset(viewsets.ModelViewSet):
                 user = User.objects.create(
                     username=uuid.uuid1(), user_type=User.INTERNAL_USER)
                 token = Token.objects.create(user=user)
-                print('################',token)
+                print('################', token)
                 internal_user_obj.user = user
                 internal_user_obj.save()
 
                 # send_response(status=status.HTTP_200_OK, developer_message="User Created")
 
-            # send_response(status=status.HTTP_400_BAD_REQUEST, developer_message="User already exists")    
+            # send_response(status=status.HTTP_400_BAD_REQUEST, developer_message="User already exists")
 
         else:
             # Invalidate previous OTPs
@@ -69,11 +70,11 @@ class UserAuthOTPViewset(viewsets.ModelViewSet):
             apnibus_logger.info(otp, mobile)
             if new_otp_created:
                 serializer.save(user=user_obj, otp=otp)
-            return send_response(status=status.HTTP_200_OK, developer_message='Request was successful')
+            return send_response(status=status.HTTP_200_OK, error_msg={}, developer_message='Request was successful', response_message='Success')
 
         else:
             return send_response(status=status.HTTP_200_OK, error=serializer.errors,
-                                 developer_message='Request failed due to invalid data.')
+                                 developer_message='Request failed due to invalid data.', response_message='Error')
 
     @action(methods=['put'], detail=False)
     def verify(self, request, *args, **kwargs):
@@ -81,9 +82,7 @@ class UserAuthOTPViewset(viewsets.ModelViewSet):
         otp = request.data.get('otp')
         apnibus_logger.info(mobile, otp)
 
-        print('################## ',mobile, otp)
-
-
+        print('################## ', mobile, otp)
 
         # db_master_otp = MasterOTP.objects.filter(
         #     otp_type=MasterOTP.CONDUCTOR_SE).last().otp
@@ -109,19 +108,19 @@ class UserAuthOTPViewset(viewsets.ModelViewSet):
             # valid_otp.is_verified = True
             # valid_otp.save()
 
-            return send_response(status=status.HTTP_200_OK, ui_message='message',
+            return send_response(status=status.HTTP_200_OK,
                                  developer_message='Request was successful.', data=data)
         else:
             valid_otp = UserAuthenticationOTP.objects.filter(mobile=mobile, otp=otp, is_valid=True,
                                                              is_verified=False).first()
-            print("########## 1",valid_otp.otp,'#########')
+            print("########## 1", valid_otp.otp, '#########')
             if valid_otp.otp:
                 internal_team_user = SalesTeamUser.objects.get(mobile=mobile)
                 user = internal_team_user.user
-                print("########## 2",user,'#########')
+                print("########## 2", user, '#########')
                 auth_token = Token.objects.get(user=user).key
                 data = {}
-                print("########## 3",auth_token,'#########')
+                print("########## 3", auth_token, '#########')
                 data['auth_token'] = auth_token
                 data['name'] = internal_team_user.name
                 data['mobile'] = internal_team_user.mobile
@@ -132,13 +131,12 @@ class UserAuthOTPViewset(viewsets.ModelViewSet):
                 valid_otp.is_verified = True
                 valid_otp.save()
 
-                return send_response(status=status.HTTP_200_OK, ui_message='message',
-                                     developer_message='Request was successful.', data=data)
-            return send_response(status=status.HTTP_401_UNAUTHORIZED,
-                                 developer_message='Invalid otp or mobile number')
+                return send_response(status=status.HTTP_200_OK, error_msg={},
+                                     developer_message='Request was successful.', data=data, response_message='Success')
+            return send_response(status=status.HTTP_401_UNAUTHORIZED, error_msg={},
+                                 developer_message='Invalid otp or mobile number', response_message='Error')
 
 
 class CreateSalesUser(viewsets.ModelViewSet):
     queryset = SalesTeamUser.objects.all()
     serializer_class = SalesTeamDataSerializer
-    
